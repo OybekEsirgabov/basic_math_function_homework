0f0e173fe9f65f356aee1314a18e51f3e8742f33
oybekEsirgabov <90684441+tillabola@users.noreply.github.com>
2022-03-17 18:10:23 +0500

    4

--- basic_func14.py
+++ basic_func14.py
@@ -5,8 +5,12 @@ from math import floor
 
 
 def main(a,b):
-    import math
-    return  math.floor(a)
-    import math
-    return math.floor(b) 
+    from math import floor
+    return (floor(a), floor(b))
+    #from math import floor
+    #return floor(b)
+    #import math
+    #return  math.floor(a)
+    #import math
+    #return math.floor(b) 
 print(main(-9.12, -15.9))
